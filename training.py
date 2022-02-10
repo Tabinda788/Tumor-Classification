a936c5f02ce60f8fea445f5499bbb578057ebf21
Tabinda788 <tabindabhat21@gmail.com>
2022-02-10 11:52:59 +0530

    adding tumor classification project

---
+++ training.py
@@ -0,0 +1,140 @@
+import tensorflow as tf
+from tensorflow.keras import models, layers
+import matplotlib.pyplot as plt
+from IPython.display import HTML
+import numpy as np
+BATCH_SIZE = 32
+IMAGE_SIZE = 630
+CHANNELS=3
+EPOCHS=50
+
+dataset = tf.keras.preprocessing.image_dataset_from_directory(
+    "brain_tumor_dataset",
+    seed=123,
+    shuffle=True,
+    image_size=(IMAGE_SIZE,IMAGE_SIZE),
+    batch_size=BATCH_SIZE
+)
+
+class_names = dataset.class_names
+class_names
+
+def get_dataset_partitions_tf(ds, train_split=0.8, val_split=0.1, test_split=0.1, shuffle=True, shuffle_size=10000):
+    assert (train_split + test_split + val_split) == 1
+    
+    ds_size = len(ds)
+    
+    if shuffle:
+        ds = ds.shuffle(shuffle_size, seed=12)
+    
+    train_size = int(train_split * ds_size)
+    val_size = int(val_split * ds_size)
+    
+    train_ds = ds.take(train_size)    
+    val_ds = ds.skip(train_size).take(val_size)
+    test_ds = ds.skip(train_size).skip(val_size)
+    
+    return train_ds, val_ds, test_ds
+
+
+train_ds, val_ds, test_ds = get_dataset_partitions_tf(dataset)
+
+
+train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=tf.data.AUTOTUNE)
+val_ds = val_ds.cache().shuffle(1000).prefetch(buffer_size=tf.data.AUTOTUNE)
+test_ds = test_ds.cache().shuffle(1000).prefetch(buffer_size=tf.data.AUTOTUNE)
+
+resize_and_rescale = tf.keras.Sequential([
+  layers.experimental.preprocessing.Resizing(IMAGE_SIZE, IMAGE_SIZE),
+  layers.experimental.preprocessing.Rescaling(1./255),
+])
+
+
+data_augmentation = tf.keras.Sequential([
+  layers.experimental.preprocessing.RandomFlip("horizontal_and_vertical"),
+  layers.experimental.preprocessing.RandomRotation(0.2),
+])
+
+
+train_ds = train_ds.map(
+    lambda x, y: (data_augmentation(x, training=True), y)
+).prefetch(buffer_size=tf.data.AUTOTUNE)
+
+
+input_shape = (BATCH_SIZE, IMAGE_SIZE, IMAGE_SIZE, CHANNELS)
+n_classes = 2
+
+model = models.Sequential([
+    resize_and_rescale,
+    layers.Conv2D(32, kernel_size = (3,3), activation='relu', input_shape=input_shape),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(64,  kernel_size = (3,3), activation='relu'),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(64,  kernel_size = (3,3), activation='relu'),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(64, (3, 3), activation='relu'),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(64, (3, 3), activation='relu'),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(64, (3, 3), activation='relu'),
+    layers.MaxPooling2D((2, 2)),
+    layers.Flatten(),
+    layers.Dense(64, activation='relu'),
+    layers.Dense(n_classes, activation='softmax'),
+])
+
+model.build(input_shape=input_shape)
+
+model.summary()
+
+model.compile(
+    optimizer='adam',
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False),
+    metrics=['accuracy']
+)
+
+history = model.fit(
+    train_ds,
+    batch_size=BATCH_SIZE,
+    validation_data=val_ds,
+    verbose=1,
+    epochs=50,
+)
+
+
+scores = model.evaluate(test_ds)
+
+print(scores)
+
+
+def predict(model, img):
+    img_array = tf.keras.preprocessing.image.img_to_array(images[i].numpy())
+    img_array = tf.expand_dims(img_array, 0)
+
+    predictions = model.predict(img_array)
+
+    predicted_class = class_names[np.argmax(predictions[0])]
+    confidence = round(100 * (np.max(predictions[0])), 2)
+    return predicted_class, confidence
+
+
+plt.figure(figsize=(15, 15))
+for images, labels in test_ds.take(1):
+    for i in range(9):
+        ax = plt.subplot(3, 3, i + 1)
+        plt.imshow(images[i].numpy().astype("uint8"))
+        
+        predicted_class, confidence = predict(model, images[i].numpy())
+        actual_class = class_names[labels[i]] 
+        
+        plt.title(f"Actual: {actual_class},\n Predicted: {predicted_class}.\n Confidence: {confidence}%")
+        
+        plt.axis("off")
+
+
+import os
+#model_version=max([int(i) for i in os.listdir("C:\\Users\\adeel\\OneDrive\\Documents\\model") + [0]])+1
+model.save(f"saved_model_final_now")
+
+
+MODEL = tf.keras.models.load_model(f"saved_model_final_now")
---
+++ try.py
@@ -0,0 +1,3 @@
+import tensorflow as tf
+
+model = tf.keras.models.load_model(f"C:\\Users\\adeel\\OneDrive\\Documents\\TumorClassification\\api\\saved_model_final_now")
